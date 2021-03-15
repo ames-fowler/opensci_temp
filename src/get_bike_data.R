9676e548f4e6e4fcbce9a9523d83b01b9c90d36d
ames-fowler <ames.fowler@esu.edu>
2021-03-15 09:32:05 -0700

    Source bike data via google drive

---
+++ src/get_bike_data.R
@@ -0,0 +1,10 @@
+# data fo bike user records
+#
+# a- Ames Fowler 
+# d- 210315
+#
+##
+
+# can't download by R need to do it by hand. 
+# loaded it from google drive manually
+# https://drive.google.com/drive/u/1/folders/1jcpZzFSro4r48y_2JqWVXbZ2rJMByedI
